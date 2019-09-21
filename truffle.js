e0a5c527e8015929daca12c88721f254bf69062d
AmadeusGB <1069830494@qq.com>
2019-09-21 13:53:48 +0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      gas: 6699900,
+      gas: 68000000,
       network_id: "*" // Match any network id
     }
   }
